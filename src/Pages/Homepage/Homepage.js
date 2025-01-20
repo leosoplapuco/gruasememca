be261a62b094b4befa69e6febff8405e673b3a6a
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-01-20 09:22:48 -0500

    1.0.7

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -64,7 +64,8 @@ const Homepage = () => {
 
                         <div className="hero-target hero-target-2">
                             <div className="hero-target-video-container">
-                                <iframe src="https://www.youtube.com/embed/L0AuNQEh5DM?si=jWNI9k355_dlTj_w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWKHDEwDsYLHEsR-c9FEm-nRp6L9n6vW8I7g&s"></img>
+                                {/* <iframe src="https://www.youtube.com/embed/L0AuNQEh5DM?si=jWNI9k355_dlTj_w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                             </div>
                         </div>
                     </section>
