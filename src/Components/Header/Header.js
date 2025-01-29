ccecaa051d2a5be2b9a6b82c86a587d8d991a067
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-01-28 22:16:09 -0500

    1.2.0

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,47 +1,27 @@
 import './Header.css';
-
 import DarkButton from '../DarkButton/DarkButton';
 import Languages from '../Languages/Languages';
-
-// import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-function Header(){
+function Header() {
     const { t } = useTranslation('header');
+    const [openMenu, setOpenMenu] = useState(null);
 
-    return(
+    const toggleSubMenu = (id) => {
+        setOpenMenu(openMenu === id ? null : id);
+    };
+
+    return (
         <header>
             <div className='header'>
                 <div className='header-top-container'>
                     <div className='header-top'>
-                        {/* <ul className='social-networks'>
-                            <li>
-                                <a href={t('socialNetworks.0.href')} title={t('socialNetworks.0.title')} target='_blank'>
-                                    <img src="https://gruasememca.vercel.app/assets/images/icons/social-networks/facebook.svg" alt={t('socialNetworks.0.title')} width={16} height={16} />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.facebook.com/gruasememcasac08" title="Nuestro perfil de Instagram" target='_blank'>
-                                    <img src="https://gruasememca.vercel.app/assets/images/icons/social-networks/instagram.svg" alt={t('socialNetworks.0.title')} width={16} height={16} />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.facebook.com/gruasememcasac08" title="Nuestro canal de YouTube" target='_blank'>
-                                    <img src="https://gruasememca.vercel.app/assets/images/icons/social-networks/youtube.svg" alt={t('socialNetworks.0.title')} width={16} height={16} />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.facebook.com/gruasememcasac08" title="Nuestro perfil en LinkedIn" target='_blank'>
-                                    <img src="https://gruasememca.vercel.app/assets/images/icons/social-networks/linkedin.svg" alt={t('socialNetworks.0.title')} width={16} height={16} />
-                                </a>
-                            </li>
-                        </ul> */}
-
                         <ul className="social-networks">
                             {t('socialNetworks', { returnObjects: true }).map((socialNetwork) => (
                                 <li key={socialNetwork.id}>
-                                    <a href={socialNetwork.href}>
-                                        <img src={socialNetwork.imgSrc} width={20} height={20} />
+                                    <a href={socialNetwork.href} className='social-network' title={socialNetwork.title}>
+                                        <img src={socialNetwork.imgSrc} width={20} height={20} alt={socialNetwork.imgAlt} />
                                     </a>
                                 </li>
                             ))}
@@ -50,19 +30,19 @@ function Header(){
                         <ul className='header-contact'>
                             <li>
                                 <a href={t('headerTopContactLinks.1.href')} title={t('headerTopContactLinks.1.title')}>
-                                    <span className="material-icons icon-small">mail</span>
-                                    <p className='font-small'>{t('headerTopContactLinks.1.text')}</p>
+                                    <span className="material-icons">mail</span>
+                                    <p>{t('headerTopContactLinks.1.text')}</p>
                                 </a>
                             </li>
                         </ul>
 
                         <p className='color-white'>|</p>
 
-                        <Languages/>
+                        <Languages />
 
                         <p className='color-white'>|</p>
 
-                        <DarkButton/>
+                        <DarkButton />
                     </div>
                 </div>
 
@@ -74,28 +54,45 @@ function Header(){
 
                         <nav className='menu-container'>
                             <ul className="menu">
-                                {t('menuLinks', { returnObjects: true }).map((menuLink) => (
-                                    <li key={menuLink.id}>
-                                        {menuLink.id === 2 || menuLink.id === 3 ? (
-                                            <button className={`menu-link menu-link-${menuLink.id}`}>
-                                                <h2>{menuLink.h2}</h2>
-                                                <span className="material-icons">keyboard_arrow_down</span>
-                                            </button>
+                                {t("menuLinks", { returnObjects: true }).map((menuLink) => (
+                                    <li key={menuLink.id} className="menu-item">
+                                        {menuLink.subMenu ? (
+                                            <>
+                                                <button className={`menu-link menu-link-${menuLink.id}`} onClick={() => toggleSubMenu(menuLink.id)}>
+                                                    <h2>{menuLink.h2}</h2>
+
+                                                    <span className="material-icons">
+                                                        {openMenu === menuLink.id ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                                                    </span>
+                                                </button>
+
+                                                {openMenu === menuLink.id && (
+                                                    <div className="sub-menu-container">
+                                                        {menuLink.subMenu.map((sub) => (
+                                                            <div key={sub.id} className="sub-menu">
+                                                                <div className="sub-menu-header">
+                                                                    <p className="sub-menu-title">{sub.title}</p>
+                                                                    <p className="sub-menu-resume text">{sub.resume}</p>
+                                                                </div>
+                                                                <ul className="sub-menu-links">
+                                                                    {sub.links.map((link) => (
+                                                                        <li key={link.id} className="sub-menu-item">
+                                                                            <a href={link.href} className="sub-menu-link">
+                                                                                <h3>{link.h3}</h3>
+                                                                                <p className='text'>{link.text}</p>
+                                                                            </a>
+                                                                        </li>
+                                                                    ))}
+                                                                </ul>
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                )}
+                                            </>
                                         ) : (
                                             <a href={menuLink.href} className={`menu-link menu-link-${menuLink.id}`}>
                                                 <h2>{menuLink.h2}</h2>
                                             </a>
-                                        )}
-                                        {menuLink.subMenu && (
-                                            <div className="sub-menu-container">
-                                                <ul className="sub-menu">
-                                                    {menuLink.subMenu.map((subLink) => (
-                                                        <li key={subLink.id}>
-                                                            <p>{subLink.title}</p>
-                                                        </li>
-                                                    ))}
-                                                </ul>
-                                            </div>
                                         )}
                                     </li>
                                 ))}
