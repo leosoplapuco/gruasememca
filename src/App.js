10085a2adde2dbeab3a421c98f26c00ecfea4b62
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-05-09 21:52:04 -0500

    0.0.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+// import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Homepage from './Pages/Homepage/Homepage';
+// import About from './components/About';
+// import NotFound from './components/NotFound';
+
+function App(){
+    return(
+        <>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                {/* <Route path="/about" element={<About />} /> */}
+                {/* Ruta comodín para páginas no encontradas */}
+                {/* <Route path="*" element={<NotFound />} /> */}
+            </Routes>
+        </>
+    );
 }
+
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <>
+            <header>
+                <p>gruasememca</p>
+            </header>
+        </>
+    )
+}
+
+export default Header;
