adb43143013fadaaa18909b4a96fc3f7a3bf9cbc
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-12-15 22:15:17 -0500

    0.0.6

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import './Home.css'
-// Aquí podrías importar composables, stores, etc.
+import type ServicesVue from './components/Services/Services.vue'
+
+import Services from './components/Services/Services.vue'
 </script>
 
 <template>
@@ -15,35 +17,25 @@ import './Home.css'
   <div class="hero-container">
     <section class="hero-content">
       <span>Grúas Ememca SAC</span>
-      <h1>Alquiler de grúas</h1>
+      <h1 class="anton-regular">Alquiler de grúas</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus deserunt debitis.
         Perspiciatis, veniam sequi!
       </p>
 
       <div class="d-flex gap-10">
-        <a href="" class="button-link button-link-1">
-          <p class="button-link-text">Saber más</p>
+        <a href="/nosotros/" class="button-link button-link-1">
+          <span class="material-symbols-outlined">group</span>
+          <p class="button-link-text">Sobre nosotros</p>
         </a>
 
         <a href="" class="button-link button-link-2">
+          <span class="material-symbols-outlined">call</span>
           <p class="button-link-text">Llamar ahora</p>
         </a>
       </div>
     </section>
   </div>
 
-  <div class="block-container">
-    <section class="block-content">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nulla illum eos
-      libero eum cum rem nobis, atque exercitationem, iure a explicabo et repudiandae dicta odio,
-      commodi quis architecto distinctio provident tempora suscipit? In illum doloribus, ullam
-      temporibus praesentium quibusdam debitis tenetur, sed rem possimus harum perspiciatis magni
-      deleniti. Est corrupti sed numquam eveniet temporibus voluptate ut molestiae veritatis alias
-      corporis animi nesciunt, voluptatem tenetur tempore! Tempora, est accusamus. Facilis
-      blanditiis, repellat sint aliquam est excepturi voluptate, alias atque qui nostrum
-      praesentium, quos earum tenetur perspiciatis neque. Laudantium velit quae a laboriosam animi
-      eaque impedit. Nulla illo voluptatem quasi labore.
-    </section>
-  </div>
+  <Services />
 </template>
---
+++ src/pages/Home/components/Services/Services.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import './Services.css'
+</script>
+
+<template>
+  <div class="block-container">
+    <section class="block-content">
+      <div class="block-title-container">
+        <span class="block-title-span">Nuestros</span>
+        <p class="block-title">Servicios</p>
+      </div>
+
+      <div class="homepage-services-buttons-container">
+        <ul>
+          <li>
+            <button class="h-p-button h-p-button-1">
+              <p>Alquiler de grúas</p>
+            </button>
+          </li>
+          <li>
+            <button class="h-p-button h-p-button-2">
+              <p>Izaje</p>
+            </button>
+          </li>
+          <li>
+            <button class="h-p-button h-p-button-3">
+              <p>Montaje y desmontaje</p>
+            </button>
+          </li>
+          <li>
+            <button class="h-p-button h-p-button-4">
+              <p>Eliminación de tierra</p>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
