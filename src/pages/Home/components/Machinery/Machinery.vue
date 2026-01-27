8a57d02e82c47fc9c1abe50f76ab28778f88a96b
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2026-01-26 22:37:30 -0500

    0.1.0

--- src/pages/Home/components/Machinery/Machinery.vue
+++ src/pages/Home/components/Machinery/Machinery.vue
@@ -6,7 +6,51 @@ import './Machinery.css'
   <div class="block-container machinery">
     <img src="https://www.gruasememca.com/media/img/gallery-1.webp" alt="" />
     <section class="block-content machinery">
-      <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, quaerat?</p>
+      <p class="homepage-machinery-name anton-regular">
+        Grove <span class="anton-regular">80</span>
+      </p>
+
+      <ul class="homepage-machinery-list homepage-machinery-list-1" id="">
+        <li>
+          <p>Marca</p>
+          <p>Marca de la grúa</p>
+        </li>
+        <li>
+          <p>Modelo</p>
+          <p>Modelo de la grúa</p>
+        </li>
+        <li>
+          <p>Año</p>
+          <p>1996</p>
+        </li>
+      </ul>
+      <ul class="homepage-machinery-list homepage-machinery-list-2" id="">
+        <li>
+          <p>Motor</p>
+          <p>Mercedes Benz</p>
+        </li>
+        <li>
+          <p>Combustible</p>
+          <p>Petroleo</p>
+        </li>
+        <li>
+          <p>Pluma</p>
+          <p>30 metros</p>
+        </li>
+      </ul>
+
+      <div class="homepage-machinery-text">
+        <p class="color-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores inventore cupiditate
+          voluptate libero aspernatur quae perspiciatis dolorum voluptatem, consectetur aut suscipit
+          voluptatum praesentium quod, rem facere! Ratione, soluta.
+        </p>
+
+        <a href="/" title="" class="button-link button-link-1">
+          <span class="material-symbols-outlined">forklift</span>
+          <p class="button-link-text">Ver flota completa</p>
+        </a>
+      </div>
 
       <img src="https://www.gruasememca.com/media/img/gruas/Demag_AC_205.webp" alt="" />
     </section>
